760a196c681b2a62d87bbd05323e0cc2ad086506
mahanadh <mahan2054@gmail.com>
2020-12-12 18:52:20 +0545

    Changed the email

--- cypress/integration/myTests/firstTest.js
+++ cypress/integration/myTests/firstTest.js
@@ -12,6 +12,6 @@ describe('My First Test', () => {
     // Get an input, type into it and verify that the value has been updated
     cy.get('.action-email')
       .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
+      .should('have.value', 'fate@email.com')
   })
 })
